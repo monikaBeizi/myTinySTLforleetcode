f26baeccf65662a5cd5dbdfa0868df2169481919
monikaBeizi <94218964+monikaBeizi@users.noreply.github.com>
2023-03-06 23:09:55 +0800

    插入排序
    没想到for循环后在for语句中定义的变量的内存会释放，让编译器以为那个变量没有被定义。这玩意整了我半天，先传个有bug 的

---
+++ tinySTL/nums.h
@@ -0,0 +1,56 @@
+#ifndef NUMSP1_H_
+#define NUMSP1_H_
+
+#include<iostream>
+
+template<class Type,size_t N>
+void insertSortPositive(Type (& arr)[N])
+{
+
+    //N是整个数组的大小
+    int i;
+
+    for(i = 1;i < N; i++)
+    {
+        if(arr[i-1]>arr[i])
+        {
+            int tempForj = 0;
+            int temp = arr[i];
+            for( int j = i -1;j>=0 && temp < arr[j];j--)//这里用temp而不是用arr[i]可以防止在前移的过程中，arr[i]的数值被改变而影响结果
+            {
+                arr[j+1] = arr[j];
+                if(j-1 >= 0 && temp < arr[j -1])
+                {
+                    tempForj = j -1;
+                }
+            }
+            arr[tempForj+1] = temp;
+        }
+    }
+}
+
+template<class Type,size_t N>
+void insertSortReverse(Type (&arr)[N])
+{
+    int i;
+    for(i = 1;i < N;i ++)
+    {
+        if(arr[i-1]<arr[i])
+        {
+            int temp=arr[i];
+            int tempForj = 0;
+            for(int j = i-1;j>= 0 && temp > arr[j];j--)
+            {
+                arr[j+1] = arr[j];
+                if(j - 1>=0 && temp > arr[j -1])
+                {
+                    tempForj =j-1;
+                }
+            }
+            arr[tempForj+1] = temp;
+        }
+    }
+}
+
+
+#endif
