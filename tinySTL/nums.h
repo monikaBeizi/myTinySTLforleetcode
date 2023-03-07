29d6c1900e5bf13a52e24a32e789df9a99f969ac
monikaBeizi <94218964+monikaBeizi@users.noreply.github.com>
2023-03-07 12:47:58 +0800

    修完了bug，准确的说是不知道为什么bug自己没了
    不知道为什么昨天明明在for循环作用域外定义了变量还是被释放了，今天定义却没有被释放。崩溃了

--- tinySTL/nums.h
+++ tinySTL/nums.h
@@ -3,28 +3,69 @@
 
 #include<iostream>
 
+// template<class Type,size_t N>
+// void insertSortPositive(Type (& arr)[N])
+// {
+
+//     //N是整个数组的大小
+//     int i;
+
+//     for(i = 1;i < N; i++)
+//     {
+//         if(arr[i-1]>arr[i])
+//         {
+//             int tempForj = 0;
+//             int temp = arr[i];
+//             for( int j = i -1;j>=0 && temp < arr[j];j--)//这里用temp而不是用arr[i]可以防止在前移的过程中，arr[i]的数值被改变而影响结果
+//             {
+//                 arr[j+1] = arr[j];
+//                 if(j-1 >= 0 && temp < arr[j -1])
+//                 {
+//                     tempForj = j -1;
+//                 }
+//             }
+//             arr[tempForj+1] = temp;
+//         }
+//     }
+// }
+
+// template<class Type,size_t N>
+// void insertSortReverse(Type (&arr)[N])
+// {
+//     int i;
+//     for(i = 1;i < N;i ++)
+//     {
+//         if(arr[i-1]<arr[i])
+//         {
+//             int temp=arr[i];
+//             int tempForj = 0;
+//             for(int j = i-1;j>= 0 && temp > arr[j];j--)
+//             {
+//                 arr[j+1] = arr[j];
+//                 if(j - 1>=0 && temp > arr[j -1])
+//                 {
+//                     tempForj =j-1;
+//                 }
+//             }
+//             arr[tempForj+1] = temp;
+//         }
+//     }
+// }
+
 template<class Type,size_t N>
-void insertSortPositive(Type (& arr)[N])
+void insertSortPositive(Type (&arr)[N])
 {
-
-    //N是整个数组的大小
-    int i;
-
-    for(i = 1;i < N; i++)
+    int i,j,temp;
+    for(i = 1;i < N; i ++)
     {
-        if(arr[i-1]>arr[i])
+        if(arr[i]<arr[i -1])
         {
-            int tempForj = 0;
-            int temp = arr[i];
-            for( int j = i -1;j>=0 && temp < arr[j];j--)//这里用temp而不是用arr[i]可以防止在前移的过程中，arr[i]的数值被改变而影响结果
+            temp = arr[i];
+            for( j =i -1;j >=0 && arr[j] > temp;--j)
             {
-                arr[j+1] = arr[j];
-                if(j-1 >= 0 && temp < arr[j -1])
-                {
-                    tempForj = j -1;
-                }
+                arr[j+1]=arr[j];
             }
-            arr[tempForj+1] = temp;
+            arr[j+1] = temp;
         }
     }
 }
@@ -32,22 +73,17 @@ void insertSortPositive(Type (& arr)[N])
 template<class Type,size_t N>
 void insertSortReverse(Type (&arr)[N])
 {
-    int i;
-    for(i = 1;i < N;i ++)
+    int i,j,temp;
+    for(i = 1;i < N;i++)
     {
-        if(arr[i-1]<arr[i])
+        if(arr[i]>arr[i -1])
         {
-            int temp=arr[i];
-            int tempForj = 0;
-            for(int j = i-1;j>= 0 && temp > arr[j];j--)
+            temp = arr[i];
+            for(j=i-1;j>=0 && arr[j] < temp;--j)
             {
-                arr[j+1] = arr[j];
-                if(j - 1>=0 && temp > arr[j -1])
-                {
-                    tempForj =j-1;
-                }
+                arr[j+1]=arr[j];
             }
-            arr[tempForj+1] = temp;
+            arr[j+1] = temp;
         }
     }
 }
